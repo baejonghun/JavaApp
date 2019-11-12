7236b4b6d64b7bba22c91e052ef4b05e065a124f
GD6 <GD6@DESKTOP-58VHOFH>
2019-11-12 15:24:14 +0900

    조건문 시작

---
+++ src/kr/gudi/conditional/조건문.java
@@ -0,0 +1,20 @@
+package kr.gudi.conditional;
+
+public class 조건문 {
+
+	public static void main(String[] args) {
+		
+		int a = 2;
+		boolean c = (1 == a);
+		
+		if(1 == a) {
+			System.out.println("참!!!");
+		} else {
+			System.out.println("거짓이다");
+		}
+		
+		System.out.println( (1 == a) ? "참" : "거짓" );
+		
+	}
+
+}
