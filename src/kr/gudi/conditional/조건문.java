87e08cf5d044a4f94699fab8e104b51f0d7393f4
GD6 <GD6@DESKTOP-58VHOFH>
2019-11-12 16:16:05 +0900

    조건문 두번째 이야기

--- src/kr/gudi/conditional/조건문.java
+++ src/kr/gudi/conditional/조건문.java
@@ -4,16 +4,26 @@ public class 조건문 {
 
 	public static void main(String[] args) {
 		
-		int a = 2;
+		int a = 0;
 		boolean c = (1 == a);
 		
-		if(1 == a) {
-			System.out.println("참!!!");
-		} else {
-			System.out.println("거짓이다");
-		}
+//		if(1 == a) {
+//			System.out.println("참!!!");
+//		} else {
+//			System.out.println("거짓이다");
+//		}
+//		
+//		System.out.println( (1 == a) ? "참" : "거짓" );
+		String b = "";
 		
-		System.out.println( (1 == a) ? "참" : "거짓" );
+		switch (b) {
+			case "":
+				System.out.println("빈값");
+				break;
+			default:
+				
+				break;
+		}
 		
 	}
 
