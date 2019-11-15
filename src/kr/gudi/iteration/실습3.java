1e1768c0f613105607168e57db928b3ef65b660b
GD6 <GD6@DESKTOP-58VHOFH>
2019-11-15 10:43:40 +0900

    while > for 변경

--- src/kr/gudi/iteration/실습3.java
+++ src/kr/gudi/iteration/실습3.java
@@ -4,15 +4,16 @@ public class 실습3 {
 
 	public static void main(String[] args) {
 		
-		for(int a = 1; a < 10; a++) { // 1 ~ 9
+		
 			
 			int b = 1;
 			while(b < 10) { // 1 ~ 9
-				System.out.println(a + " * " + b + " = " + (a * b));
+				for(int a = 1; a < 10; a++) { // 1 ~ 9
+					System.out.println(b + " * " + a + " = " + (a * b));
+				}
 				b++;
 			}
 			
-		}
 		
 		
 		
