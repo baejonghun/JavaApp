e2dad5a27609827d9557b2d53b6278d0110cd09c
GD6 <GD6@DESKTOP-58VHOFH>
2019-11-15 10:38:00 +0900

    반복문 실습 3 초기

---
+++ src/kr/gudi/iteration/실습3.java
@@ -0,0 +1,19 @@
+package kr.gudi.iteration;
+
+public class 실습3 {
+
+	public static void main(String[] args) {
+		
+		for(int a = 1; a < 10; a++) { // 1 ~ 9
+			
+		}
+		
+		int b = 1;
+		while(b < 10) { // 1 ~ 9
+			
+			b++;
+		}
+		
+	}
+
+}
